6531bfd9fb38e1582b1316fc0dd34b67d2684240
Timotheus Kampik <timotheus.kampik@gmail.com>
2019-12-09 15:32:29 +0100

    get grandparent path

--- hand_in/tkampik_jcnieves/test_runner.py
+++ hand_in/tkampik_jcnieves/test_runner.py
@@ -2,6 +2,7 @@ import os,sys,inspect
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
+sys.path.insert(0, os.path.dirname(parent_dir))
 
 import bandit
 
