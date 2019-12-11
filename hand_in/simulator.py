bdc3d9e6a869f0ebc94e7d504f0bb460d8cf642a
Timotheus Kampik <timotheus.kampik@gmail.com>
2019-12-11 16:20:19 +0100

    add deviation, fix shuffling issue

--- hand_in/simulator.py
+++ hand_in/simulator.py
@@ -2,17 +2,17 @@ from random import random
 from random import gauss
 
 def generate_reward(arm_index, expected_rewards_approx):
-    return gauss(expected_rewards_approx[arm_index], 0) + random() / 2
+    return gauss(expected_rewards_approx[arm_index], 1) + random() / 2
 
 def simulate(bandit):
     acc_rewards = [0 for _ in range(6)]
-
+    expected_rewards_approx = [
+        1 + (random() / 2) for _ in range(4)
+    ]
+    expected_rewards_approx.append(-5)
+    expected_rewards_approx.append(-10)
+    shuffle(expected_rewards_approx)
     for _ in range(10000):
-        expected_rewards_approx = [
-            1 + (random() / 2) for _ in range(4)
-        ]
-        expected_rewards_approx.append(-5)
-        expected_rewards_approx.append(-10)
         for arm_index in range(6):
             acc_rewards[arm_index] = acc_rewards[arm_index] + generate_reward(arm_index, expected_rewards_approx)
 
