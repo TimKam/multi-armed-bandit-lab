df0ab2fcf9a160adcd4bbc6e7039f751a307f87e
Maximilian Büchel <maximilian.buechel@tu-dormtund.de>
2019-12-17 11:36:43 +0100

    add custom multi-armed bandit implementation

---
+++ hand_in/tkampik_jcnieves/test_runner2.py
@@ -0,0 +1,23 @@
+import os,sys,inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+sys.path.insert(0, os.path.dirname(parent_dir))
+
+import bandit
+
+def simulate():
+    results = []
+    for _ in range(0, 20):
+        bandit_reward = bandit.simulator.simulate(bandit.bandit)
+        ref_bandit_reward = bandit.simulator.simulate(bandit.ref_bandit)
+        ref_plus_bonus = ref_bandit_reward * 1.2 #1.05
+        result = 0
+        if (bandit_reward > ref_plus_bonus):
+            result = 1
+        results.append(result)
+    return results
+
+def test_performance():
+    assert True
+    assert sum(simulate()) > 15 #17
